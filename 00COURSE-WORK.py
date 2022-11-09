f851c0289972701233243efe73910c08a9229f9f
zuzannasosnowska <ucabzrs@ucl.ac.uk>
2022-11-09 19:46:54 +0000

    call no of refugees code
    me and tania testing

--- 00COURSE-WORK.py
+++ 00COURSE-WORK.py
@@ -3,6 +3,11 @@
 
 # Docstring aren't serious docstrings but rough estimates of what certain method/class is all about
 # Comments set my immediate ideas on how to implement
+import pandas as pd
+import openpyxl
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 class CentralFunctions():
     
@@ -46,8 +51,17 @@ class CentralFunctions():
         (Potentially can be conditionally configured to also return their quantitative needs upon further request, such as food,
         water and shelter demands and how much we got in the "central hub" + graphs(!) which can be done with pandas but better done with matplotlib)
         '''
-        pass
-    
+        pd.set_option('display.max_columns',15)
+        list_of_refugee = pd.read_excel('RefugeeList.xlsx')
+        print(list_of_refugee)
+        number_of_refugee = len(list_of_refugee)
+        print("Number or refugee: " + str(number_of_refugee))
+        count_camps = list_of_refugee['Camp ID'].value_counts()
+        print(count_camps)
+        count_camps.plot.bar()
+        plt.show()
+        mental_state_count = list_of_refugee['Mental State'].value_counts()
+        print(mental_state_count)
     def call_camps(self):
         '''
         Sounds fucking evil, we might need to change the name of the method XD
@@ -59,7 +73,7 @@ class CentralFunctions():
            (> Each camp's resource capacity)
         '''
         pass
-    
+
     def call_volunteers(self):
         '''
         Reads the .csv file with volunteers and returns relevant info
@@ -67,19 +81,19 @@ class CentralFunctions():
             > How many volunteers in each camp
             > Their names
         Potentially put limiters to view volunteers from one specific camp
-        (Potentially add an option to curtail information to bare bones minimum(just username for example or just in one specific camp). Why? 
+        (Potentially add an option to curtail information to bare bones minimum(just username for example or just in one specific camp). Why?
         So we can use this method in the future for activating and deactivating volunteers in a curtailed form.
         It would be convinient to look at the list of volunteers when you type out a deletion command)
         '''
         pass
-    
+
     def login(self):
         '''
         The only interactive method in this class.
         Defines who is the current user from user input. Will be useful when we get to forming architecture for user interactions.
-        Cross refernece with .csv file 
+        Cross refernece with .csv file
         self.current_user = "vol" OR "adm"
-        
+
         (When admin logs-in a special procedure will active which might create a special volunteer account for the admin to allow the admin
         to have capabilities of volunteers for further convinience. This way we don't have to mix admin's capabilities with volunteers'
         capabilities.)
@@ -89,39 +103,39 @@ class CentralFunctions():
         '''
         # User input will be interpreted and checked via a dictionary of admins and volunteers.
         pass
-    
+
     def users(self):
         '''
         Will read the .csv file with the volunteers and form a dictionary which will be employed by login(), with passwords and logins
         '''
         pass
-    
+
     def functions(self):
         '''
         creates a dictionary of all central functions to be used by admin and volunteer classes
         '''
         pass
-    
+
 class admin(CentralFunctions):
     '''
     Functions relevant to admin user type. Have both dry no-interaction methods and interactive methods to call them for ease of readability.
     '''
-    
+
     def __init__(self):
         CentralFunctions.__init__(self)
         pass
-    
+
     def admin_functions(self): # no interaction
         '''
         creates dictionary of all no-interaction methods used by admin for later interpretation by user-input methods
         '''
         pass
-    
+
     def create_emergency(self, name_of_emergency, type, description, location, start_date, close_date): # no interaction
         '''
         Inputs will all be strings (except maybe dates if we will be allowed to use datetime module)
         Creates a FOLDER with a number of .csv files, all with relevant names.
-        self.active_emergency = 'name_of_emergency' <-- this is done so when we close the emergency it would get archieved and all 
+        self.active_emergency = 'name_of_emergency' <-- this is done so when we close the emergency it would get archieved and all
                                                         relevant data would be saved with relevant name for ease of use.
         .csv files: <-- .csv files provide PERSISTENCE
             > dataframe with emergency properties outlined in the argument
@@ -130,18 +144,18 @@ class admin(CentralFunctions):
             > dataframe with refugees (at this stage empty, but only with columns)
         '''
         pass
-    
+
     def close_emergency(self): # no interaction
         '''
         Take the folder and all the data we accumulated in the .csv files and save under a relevant name like "closed emergency XXX"
         Clear all global and local self.variables.
         Stop the entire program (I'm not even sure how to do that from a method tbh, but we'll figure it out)
-        
+
         Advantage of saving all the data under a new name is it won't possibly ever be interacted with by the rest of the program and
         we get to archive the data which is real-life-useful thing to do.
         '''
         pass
-    
+
     def admin_volunteer_commands(self, command): # no interaction
         '''
         Input will be a string which will dictate what command to apply. Why not use a dictionary? Because there will be only three available
@@ -156,23 +170,23 @@ class admin(CentralFunctions):
         # before giving or not giving access. This method would simply change that status
         # In the case of delete simply delete the record.
         pass
-    
+
     def write_volunteer(self, username, password, camp): # no interaction
         '''
         Method that will write into the volunteer dataframe adding new volunteers but only with their username and password and camp assigned to.
-        Make sure to have a method that will update the users dictionary (either by rerunning the users() method or manually adding a new entry into the 
+        Make sure to have a method that will update the users dictionary (either by rerunning the users() method or manually adding a new entry into the
         existing dictionary)
-        
+
         (When run maybe show the names of exisiting camps for user friendliness)
         '''
         pass
-    
+
     def write_camp(self, name, population_capacity, resource_capacity): # no interaction
         '''
         Write into the camps dataframe adding the camps relevant to the emergency and their properties.
         '''
         pass
-    
+
     def admin_interaction(self):
         '''
         method that will require user input and then interpret it as an executable method. This one will be tricky to design because the following functions must be
@@ -197,34 +211,34 @@ class admin(CentralFunctions):
         # 4 Once all data is logged in the local dataframe, show it to the user for confimation, ask if they are satisfied and then if they are save() it
         # 5 smart use of while loops
         pass
-    
+
 class volunteer(CentralFunctions):
     '''
     Functions relevant to volunteer user type. Have both dry no-interaction methods and interactive methods to call them for ease of readability
     '''
-    
+
     def __init__(self):
         CentralFunctions.__init__(self)
-        pass       
-    
+        pass
+
     def vol_functions(self): # no interaction
         '''
         creates dictionary of all no-interaction methods used by volunteer for later interpretation by user-input methods
         '''
         pass
-    
+
     def edit_self_info(self, name, phone, age, gender): # no interaction
         '''
         Write into the volunteer dataframe to update info on a particular volunteer
         '''
         pass
-    
+
     def create_profile(self, name, no_of_relatives, medical_needs, camp): # no interaction
         '''
         Write into the refugee dataframe to add refugees family
         '''
         pass
-    
+
     def vol_interaction(self):
         '''
         Puts the no interaction methods together to form an interaface that allows the volunteer to interact with the program. Largely similar to the admin analogue
@@ -233,7 +247,7 @@ class volunteer(CentralFunctions):
             > Basically have the same capabilities to interact as the admin
         '''
         pass
-    
+
 def execute():
     '''
     How does all of this shit work bruh
@@ -242,3 +256,6 @@ def execute():
     # call login method and methods made to check if there exists an emergency, then use logic to determine how to treat the admin and volunteers
     # depending on who the user is call relevant interaction method
     pass
+
+c = CentralFunctions()
+c.call_no_of_refugees()
