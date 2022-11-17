76aa8cf292b1cc88ebb6c711a7dfbebf2cee3617
Fedor Sulitskiy <72353528+fedorSulitskiy@users.noreply.github.com>
2022-11-17 17:37:21 +0000

    commited my fucntion to main

--- 00COURSE-WORK.py
+++ 00COURSE-WORK.py
@@ -23,7 +23,10 @@ class CentralFunctions():
     
     def __init__(self):
         self.active_emergency = None
+        self.user_data = None
         self.current_user = None
+        self.vol_data = None
+        self.camp_of_user = None
         self.read_all_data()
         self.functions()
         pass
@@ -130,22 +133,73 @@ class CentralFunctions():
         '''
         pass
 
-    def login(self):
+    def users_login(self):
         '''
-        The only interactive method in this class.
-        Defines who is the current user from user input. Will be useful when we get to forming architecture for user interactions.
-        Cross refernece with .csv file
-        self.current_user = "vol" OR "adm"
-
-        (When admin logs-in a special procedure will active which might create a special volunteer account for the admin to allow the admin
-        to have capabilities of volunteers for further convinience. This way we don't have to mix admin's capabilities with volunteers'
-        capabilities.)
-        (During login I believe admins should have a distinct procedure; creation of their account by themselves, since all the volunteer accounts
-        will be created by admins and then only filled by volunteers, so perhaps a separate method would be employed whithin this method to form
-        an account creation procedure)
+        Reads (or if first time logging in creates) users_dataframe.csv.
+        Creates self.user_data DataFrame. # when admin wants to call all details of all users they'd use this
+        Asks the user to input the login and password credentials.
+        Upon successful login will set global parameter self.current_user as either 'adm' or 'vol'.
+        
+        (IMPORTANT: considers existence of only one admin username)
         '''
-        # User input will be interpreted and checked via a dictionary of admins and volunteers.
+        # part which reads/creates the user_database file
+        try: 
+            df = pd.read_csv('user_database.csv').set_index('username')
+            df['password'] = df['password'].astype(str)
+            users_dict = df.to_dict(orient='index')
+            self.user_data = df
+        except FileNotFoundError:
+            users_dict = {'username':['admin'],'password':['111'],'role':['admin'],'activated':['TRUE']}
+            df = pd.DataFrame(users_dict)
+            df.set_index('username', inplace=True)
+            df['password'] = df['password'].astype(str)
+            df.to_csv('user_database.csv')
+            self.user_data = df
+        except:
+            print("System couldn't read your user database file.")
         pass
+        # part which reads the volunteers data
+        try:
+            df = pd.read_csv('VolounteersData.csv').set_index('Username')
+            vol_dict = df.to_dict(orient='index')
+            self.vol_data = df
+        except FileNotFoundError:
+            vol_dict = {'Username':[''],'First name':[''],'Second name':[''],'Camp ID':[''],'Avability':[''],'Status':['']}
+            df = pd.DataFrame(vol_dict)
+            df.set_index('Username', inplace=True)
+            df.to_csv('VolounteersData.csv')
+            self.vol_data = df
+        except:
+            print("System couldn't read your volunteer database file.")
+        pass
+        
+        # interactive part which checks credentials of the person attempting to login
+        while True:
+            username = input('Please input your username: ')
+            if username not in self.user_data.index:
+                print('Please input valid username')
+                continue
+            while True:
+                password = input('Please input your password: ')
+                if password == users_dict[username]['password']:
+                    if username == 'admin':
+                        self.current_user = "adm"
+                    else:
+                        self.current_user = 'vol'
+                        
+                    if self.current_user == 'adm':
+                        print(f'Welcome back {username}!')
+                        self.camp_of_user = 'adm'
+                        break
+                    else:
+                        a = vol_dict[username]['First name ']
+                        print(f'Welcome back {a}!')
+                        
+                    self.camp_of_user = vol_dict[username]['Camp ID']
+                    break
+                else:
+                    print('You entered incorrect password')
+            break
 
     def users(self):
         '''
